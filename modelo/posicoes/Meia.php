786aa30bdb0ea8be8a750934c363766e7c83c496
Thiago Dier <20251foz0020003@estudantes.ifpr.edu.br>
2026-09-23 15:43:45 -0300

    Add assistencias property and methods to Meia class

--- modelo/posicoes/Meia.php
+++ modelo/posicoes/Meia.php
@@ -1,1 +1,24 @@
-codiguinho
+<?php 
+
+class Meia extends Jogador {
+    private int $assistencias;
+
+    public function __construct(string $nome, string $nacionalidade, string $time, float $valorDeMercado, int $overall, int $assistencias)
+    {
+        $this->assistencias = $assistencias;
+    }
+
+    public function getAssistencias(): int
+    {
+        return $this->assistencias;
+    }
+
+    public function setAssistencias(int $assistencias): self
+    {
+        $this->assistencias = $assistencias;
+
+        return $this;
+    }
+}
+
+
