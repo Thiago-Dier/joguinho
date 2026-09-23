81ed7208108f383776ca655cb0c37335c6f69918
Thiago Dier <20251foz0020003@estudantes.ifpr.edu.br>
2026-09-23 15:44:16 -0300

    Update Zagueiro.php

--- modelo/posicoes/Zagueiro.php
+++ modelo/posicoes/Zagueiro.php
@@ -1,1 +1,23 @@
-codiguinho
+<?php 
+
+class Zagueiro extends Jogador{
+    private int $desarmes;
+
+    public function __construct(string $nome, string $nacionalidade, string $time, float $valorDeMercado, int $overall, int $desarmes)
+    {
+        $this->desarmes = $desarmes;
+    }
+
+    public function getDesarmes(): int
+    {
+        return $this->desarmes;
+    }
+
+    public function setDesarmes(int $desarmes): self
+    {
+        $this->desarmes = $desarmes;
+
+        return $this;
+    }
+}
+
