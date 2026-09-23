abf232dd3d64a9a9c10d321348be936239874b60
Thiago Dier <20251foz0020003@estudantes.ifpr.edu.br>
2026-09-23 15:43:17 -0300

    Add Atacante class with golsMarcados property

--- modelo/posicoes/Atacante.php
+++ modelo/posicoes/Atacante.php
@@ -1,1 +1,25 @@
-codiguinho
+<?php 
+
+class Atacante extends Jogador {
+
+    private int $golsMarcados;
+
+    #[Override]
+    public function __construct(string $nome, string $nacionalidade, string $time, float $valorDeMercado, int $overall, int $golsMarcados)
+    {
+        $this->golsMarcados = $golsMarcados;
+    }
+
+    public function getGolsMarcados(): int
+    {
+        return $this->golsMarcados;
+    }
+
+    public function setGolsMarcados(int $golsMarcados): self
+    {
+        $this->golsMarcados = $golsMarcados;
+
+        return $this;
+    }
+}
+
