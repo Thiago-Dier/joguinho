3bcca5eba0181d4c8d88ea306aba2d32e2bb0164
Thiago Dier <20251foz0020003@estudantes.ifpr.edu.br>
2026-09-23 15:42:07 -0300

    Update execucao.php

--- execucao.php
+++ execucao.php
@@ -1,1 +1,37 @@
-codiguinho
+<?php 
+
+require_once("posicoes/Atacante.php");
+require_once("posicoes/Meia.php");
+require_once("posicoes/Zagueiro.php");
+require_once("posicoes/Goleiro.php");
+require_once("Jogador.php");
+
+$Bastard = array();
+$PXG = array();
+$Ubers = array();
+$ManshineCity = array();
+$Barcha = array();
+
+//Jogadores Bastard Munchen
+$Bastard = [
+new Atacante("Noel Noa", "França(FRA) 🇫🇷​", "Bastard Munchen - 🇩🇪​", 1000000000, 99, 99), //Maior Valor e Over por ser o Master Striker do Time
+new Atacante("Michael Kaiser", "Alemanha(GER) 🇩🇪​", "Bastard Munchen - 🇩🇪​", 40000000, 97, 99),
+new Atacante("Yoichi Isagi", "Japão(JPN) 🇯🇵", "Bastard Munchen - 🇩🇪​​", 240000000, 95, 94),
+new Meia("Hiori Yo", "Japão(JPN) 🇯🇵", "Bastard Munchen - 🇩🇪​", 39000000, 84, 89),
+new Meia("Hanze Kurona", "Japão(JPN) - 🇯🇵", "Bastard Munchen - 🇩🇪​", 35000000, 90, 2),
+new Meia("Jin Kiyora", "Japão(JPN) 🇯🇵", "Bastard Munchen - 🇩🇪​", 26000000, 80, 84),
+new Zagueiro("Rensuke Kunigami","Japão(JPN) 🇯🇵", "Bastard Munchen - 🇩🇪​", 66000000, 87, 92), //Atua como zagueiro também, mas é atacante
+new Zagueiro("Gurimu Igarashi", "Japão(JPN) 🇯🇵", "Bastard Munchen - 🇩🇪​", 3000000, 79, 85),
+new Goleiro("Gin Gagamaru", "Japão(JPN) 🇯🇵", "Bastard Munchen - 🇩🇪​", 50000000, 86, 99)
+];
+
+//Jogadores PXG
+$PXG = [ 
+new Atacante("Julian Loki", "França(FRA) 🇫🇷​", "Paris X Gen - 🇫🇷​", 1000000000, 99, 99), //Maioro Valor e Over por ser o Master Striker do Time
+new Atacante("Rin Itoshi", "Japão(JPN) 🇯🇵​", "Paris X Gen - 🇫🇷​", 243000000, 97, 99),
+new Atacante("Ryusei Shido", "Japão(JPN) 🇯🇵​", "Paris X Gen - 🇫🇷​", 160000000, 93, 99),
+new Meia("Charles Chevalier", "França(FRA) 🇫🇷​​", "Paris X Gen - 🇫🇷​", 110000000, 91, 96),
+new Meia("Tabito Karasu", "Japão(JPN) 🇯🇵​​", "Paris X Gen - 🇫🇷​", 550000000, 88, 86),
+new Zagueiro("Nijjiro Nanase", "Japão(JPN) 🇯🇵​​", "Paris X Gen - 🇫🇷​", 250000000, 82, 80),
+
+];
