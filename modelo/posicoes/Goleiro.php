89d8cf7746b3b3896176a38aa4147eedae281221
Thiago Dier <20251foz0020003@estudantes.ifpr.edu.br>
2026-09-23 15:43:33 -0300

    Update Goleiro.php

--- modelo/posicoes/Goleiro.php
+++ modelo/posicoes/Goleiro.php
@@ -1,1 +1,28 @@
-codiguinho
+<?php 
+
+class Goleiro extends Jogador {
+    private int $defesas;
+
+    public function __construct(string $nome, string $nacionalidade, string $time, float $valorDeMercado, int $overall,  int $defesas)
+    {
+        $this->defesas = $defesas;
+    }
+
+    public function getDefesas(): int
+    {
+        return $this->defesas;
+    }
+
+    public function setDefesas(int $defesas): self
+    {
+        $this->defesas = $defesas;
+
+        return $this;
+    }
+}
+
+
+
+
+
+
